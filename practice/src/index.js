90ed2b7cc71b11ab8fab98a237fb7689f672b5d1
Hritik Kumar Singh <85543420+hashhritik@users.noreply.github.com>
2025-02-12 18:37:37 +0530

    Add files via upload

---
+++ practice/src/index.js
@@ -0,0 +1,195 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import Parent from "./ClassComponent/PureComponent/Parent";
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <>
+//   <Parent/>
+//   </>
+// );
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import MaterialUI from "./FunctionalComponent/MaterialUI";
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <>
+//   <MaterialUI/>
+//   </>
+// );
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import BootstrapExample from "./FunctionalComponent/BootstrapExample";
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <>
+//     <BootstrapExample />
+//   </>
+// );
+//////////////////////////////////
+
+// import React from 'react'
+// import ReactDOM from "react-dom/client"
+// import ParentF from './FunctionalComponent/SendDataPC/ParentF';
+// import ChildF from './FunctionalComponent/SendDataPC/ChildF';
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <>
+//   <ParentF/>
+//   {/* <Child/> */}
+//   </>
+// )
+
+///////////////////////////////////////////
+
+// import React from 'react'
+// import ReactDOM from "react-dom/client"
+// import Child from './ClassComponent/SendingDataPC/Child';
+// import Parent from './ClassComponent/SendingDataPC/Parent';
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <>
+//   <Parent/>
+//   {/* <Child/> */}
+//   </>
+// )
+
+///////////////////////////////////////
+
+// import React from 'react'
+// import ReactDOM from "react-dom/client"
+// import StateExample from './ClassComponent/StateExample';
+// import StateExampleF from './FunctionalComponent/StateExampleF';
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <>
+//   <StateExample/>
+//   <hr/>
+//   <StateExampleF/>
+//   </>
+// )
+
+///////////////////////////////////////////////
+// import React from 'react'
+// import ReactDOM from "react-dom/client"
+// import Test from './ClassComponent/Test';
+// import TestF from './FunctionalComponent/TestF';
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <>
+//   <Test/>
+//   <hr/>
+//   <TestF/>
+//   </>
+// )
+
+///////////////////////////////////////////
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import ModuleExample, { name1, arr1, emp1, display1 } from "./ClassComponent/ModuleExample"
+// import ModuleExampleF, { name2, arr2, emp2, display2 } from "./FunctionalComponent/ModuleExampleF"
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <>
+//     <ModuleExample />
+//     <h3>Name : {name1}</h3>
+//     <h3>Array : {arr1.join(" ")}</h3>
+//     <h3>
+//       employee details
+//       <br />
+//       ID = {emp1.id}
+//       <br />
+//       Name = {emp1.name}
+//       <br />
+//       Designation = {emp1.dsg}
+//       <br />
+//       City = {emp1.city}
+//       <br />
+//     </h3>
+//     {display1()}
+
+//     <hr />
+
+//     <ModuleExampleF />
+//     <h3>Name : {name2}</h3>
+//     <h3>Array : {arr2.join(" ")}</h3>
+//     <h3>
+//       employee details
+//       <br />
+//       ID = {emp2.id}
+//       <br />
+//       Name = {emp2.name}
+//       <br />
+//       Designation = {emp2.dsg}
+//       <br />
+//       City = {emp2.city}
+//       <br />
+//     </h3>
+//     {display2()}
+
+//     <hr />
+//   </>
+// );
+
+/////////////////////////////////////////////////////////
+
+// import React from "react";
+// import ReactDOM from "react-dom/client"
+// import * as Data from "./ClassComponent/ModuleExample"
+
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(
+//   <>
+
+//     <Data.default />
+//     <h3>Name : {Data.name1}</h3>
+//     <h3>Array : {Data.arr1.join(" ")}</h3>
+//     <h3>
+//       employee details
+//       <br/>
+//       ID = {Data.emp1.id}
+//       <br/>
+//       Name = {Data.emp1.name}
+//       <br />
+//       Designation = {Data.emp1.dsg}
+//       <br/>
+//       City = {Data.emp1.city}
+//       <br/>
+//     </h3>
+//     {Data.display1()}
+
+//      <hr/>
+//   </>
+// )
+
+///////////////////////////////////////////
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import Sample from "./ClassComponent/Sample";
+// import SampleF from "./FunctionalComponent/SampleF";
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <>
+//     <Sample />
+//     <hr/>
+//     <SampleF />
+//   </>
+// );
