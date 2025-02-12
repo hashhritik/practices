ff8d16fdcc778670f89aac037829256c9f366d16
Hritik Kumar Singh <85543420+hashhritik@users.noreply.github.com>
2025-02-12 18:46:57 +0530

    Add files via upload

---
+++ practice/javascript_practices/js_practice.js
@@ -0,0 +1,15 @@
+var a =[10,20,30,40,50,60,70,80,90,10,20,30,40,50,60,70,80,90]
+
+var b= new Set(a)
+// console.log(a);
+console.log(b);
+
+// var c= Array.from(b)
+// console.log(c);
+
+b.add(100)
+console.log(b);
+b.add(110)
+console.log(b);
+b.add(120)
+console.log(b);
